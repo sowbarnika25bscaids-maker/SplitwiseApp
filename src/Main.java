ca7e9da05e719c0f57615c03f626b5d75f180524
Sowbarnika <sowbarnika.25bscaids@kprcas.ac.in>
2026-08-10 09:50:29 +0530

    JAVA DAY 1

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Splitwise obj = new Splitwise();
+        obj.displaymsg();
+    }
+}
---
+++ src/Splitwise.java
@@ -0,0 +1,5 @@
+public class Splitwise {
+    public void displaymsg(){
+        System.out.print("Welcome to Splitwise App");
+    }
+}
